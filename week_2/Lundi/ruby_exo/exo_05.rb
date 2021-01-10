58658ecf3a00d6ad461a571792dff5cfea24fd72
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-10 17:38:19 +0100

    Update

--- week_2/Lundi/ruby_exo/exo_05.rb
+++ week_2/Lundi/ruby_exo/exo_05.rb
@@ -1,5 +1,5 @@
 puts "On va conpter le nombre d'heures de travail a THP"
-puts "Travail : #{10 * 5 * 11}"
+puts "Travail : #{10 * 5 * 11}" #will display result of multiplication
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
 
 puts "Et en secondes ?"
@@ -10,13 +10,15 @@ puts "Est-ce que c'est vreai que 3 + 2 < 5 - 7 ?"
 
 puts 3 + 2 < 5 - 7
 
-puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+# When #{} is used in the context of a regular operation, it will display the result of the operation
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" 
 puts "Ça fait combien 5 - 7 ? #{5 - 7}"
 
 puts "Ok, c'est faux alors !"
 
 puts "C'est drôle ça, faisons-en plus :"
 
+# When #{} is used with the comparaison operators, it will act as a boolean and display a true/false statement
 puts "Est-cd que 5 est plus rand que -2 ? #{5 > -2}"
 puts "Est-ce que 5 est supérieur ou égal a -2 ? #{5 >= -2}"
 puts "Est-ce que 5 est inférieur ou égal a -2 ? #{5 <= -2}"
