2cdcdbcd92b851e46b33784562996f76a72d7725
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-10 17:31:31 +0100

    Initial commit

---
+++ week_2/Lundi/ruby_exo/exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va conpter le nombre d'heures de travail a THP"
+puts "Travail : #{10 * 5 * 11}"
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60
+
+puts "Est-ce que c'est vreai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-cd que 5 est plus rand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal a -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal a -2 ? #{5 <= -2}"
