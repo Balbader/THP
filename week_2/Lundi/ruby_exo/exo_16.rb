3d0afd409a6b2d68207802017d87ca941653aa2d
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-11 20:00:16 +0100

    Update

--- week_2/Lundi/ruby_exo/exo_16.rb
+++ week_2/Lundi/ruby_exo/exo_16.rb
@@ -4,10 +4,14 @@ year = 2021
 year_of_birth = year - age
 age_of_birth = 0
 
-while age_of_birth < age
+while age_of_birth < age - 1
   puts "#{year - year_of_birth} years ago, you were #{age_of_birth}"
   year_of_birth += 1
   age_of_birth += 1
+end
+
+if year - year_of_birth == 1
+  puts "#{year - year_of_birth} year ago, you were #{age_of_birth}"
 end
 
 if age_of_birth = age
