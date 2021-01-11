87fb55f1a5295ee04714f8d37a746429b999f3b9
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-11 12:48:03 +0100

    Initial commit

---
+++ week_2/Lundi/ruby_exo/exo_17.rb
@@ -0,0 +1,18 @@
+puts "Enter date of birth: " 
+year_of_birth = gets.chomp.to_i
+year = 2021
+age = 2021 - year_of_birth
+age_of_birth = 0
+
+if year_of_birth < year
+  while year_of_birth < year
+    puts "In  #{year_of_birth} you were #{age_of_birth}"
+    year_of_birth += 1
+    age_of_birth += 1
+  end
+end
+
+if year_of_birth = year
+  puts "In #{year - age/2} you were 1/2 of your age today!"
+end
+
