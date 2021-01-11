54b8f46fec132ff0fd13e32c6b3058727c030d29
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-11 18:30:43 +0100

    Update

--- week_2/Lundi/ruby_exo/exo_16.rb
+++ week_2/Lundi/ruby_exo/exo_16.rb
@@ -1,18 +1,13 @@
-puts "Enter date of birth: " 
-year_of_birth = gets.chomp.to_i
+puts "Enter age:  " 
+age = gets.chomp.to_i
 year = 2021
-age = 2021 - year_of_birth
+year_of_birth = 2021 - age
 age_of_birth = 0
 
-if year_of_birth < year
-  while year_of_birth < year
-    puts "In  #{year_of_birth} you were #{age_of_birth}"
-    year_of_birth += 1
-    age_of_birth += 1
+  while age >= age_of_birth
+    puts "In #{year} you were #{age}"
+    year_of_birth -= 1
+    age -= 1
+    year -= 1
   end
-end
-
-if year_of_birth = year
-  puts "As of #{year} you are #{year_of_birth}"
-end
 
