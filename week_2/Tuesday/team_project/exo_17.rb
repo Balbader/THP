6e572ce26874a68b0f9b439a5192ed8ce47049e2
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-13 13:02:31 +0100

    Update

--- week_2/Tuesday/team_project/exo_17.rb
+++ week_2/Tuesday/team_project/exo_17.rb
@@ -1,20 +1,10 @@
 puts "Salut, bienvenue dans ma super pyramide ! Entre un numero entre 1 et 25 ?"
 print "> "
 nb = gets.chomp.to_i
-
-i = 0
-j = 0
-char = "#"
-if nb > 25
+puts "Voici la pyramide :"
+if nb > 25 || nb < 1
   puts "Error!"
 else
-  puts "Voici la pyramide :"
-  print " " * (nb - nb / 2)
-  while nb >= i do
-    print " " * (nb / 2)
-    puts "#{char}" * (2 * j + 1)
-    print " " * (nb / 2)
-    nb -= 1
-    j += 1
-  end
+nb.times do |n|
+  puts " " * (nb - n - 1) + "#" * (n + n + 1)
 end
