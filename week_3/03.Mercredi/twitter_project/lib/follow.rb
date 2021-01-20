444c94af2d2b93bb46ae831a10ae62c3fc66a5a4
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-20 21:03:00 +0100

    update

--- week_3/03.Mercredi/twitter_project/lib/follow.rb
+++ week_3/03.Mercredi/twitter_project/lib/follow.rb
@@ -1,13 +1,19 @@
 require 'twitter'
 require 'dotenv'
 
-Dotenv.load('../.env') 
+Dotenv.load('../.env')
 
-# quelques lignes qui appellent les clés d'API de ton fichier .env
-  client = Twitter::REST::Client.new do |config|
+client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
     config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
     config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
     config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-  end
-  client.update('Mon premier tweet en Ruby !!!!')
+end
+
+i = 0
+while i < 25
+  client.search("#bonjour_monde", lang: "fr", result_type: "recent")
+  id = client.user("#bonjour_monde")
+  client.follow("#bonjour_monde")
+  i += 1
+end
--- week_3/03.Mercredi/twitter_project/lib/like.rb
+++ week_3/03.Mercredi/twitter_project/lib/like.rb
@@ -1,13 +1,15 @@
 require 'twitter'
 require 'dotenv'
 
-Dotenv.load('../.env') 
+Dotenv.load('../.env')
 
-# quelques lignes qui appellent les clés d'API de ton fichier .env
-  client = Twitter::REST::Client.new do |config|
+client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
     config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
     config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
     config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
   end
-  client.update('Mon premier tweet en Ruby !!!!')
+
+  like = client.search("#bonjour_monde", lang: "fr", result_type: "recent").take(25)
+  client.favorite(like)
+
