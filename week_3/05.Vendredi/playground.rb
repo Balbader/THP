fbb55330647eff8f4a4763d00dcc5a4c80e76950
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-22 15:42:18 +0100

    update

--- week_3/05.Vendredi/playground.rb
+++ week_3/05.Vendredi/playground.rb
@@ -1,22 +1,27 @@
+#--------------- Using user input aka "gets.chomp"
+
+
 file = File.open("/Users/basilalbader/dev/the_hacking_project/THP/week_3/05.Vendredi/Gemfile", "w")
-
-contenu = file.puts("source 'https://rubygems.org'\ngem'pry'")
-
+contenu = file.puts("source 'https://rubygems.org'\n")
 puts "add gem"
-
-#get user input
 input = gets.chomp
-
-#get argument value
-input = ARGV.join()
-#note: never use "gets.chomp" and "ARGV" together as it will crash the program. 
-#In case you need both in your program (you moron!!!) make sure to use the following command after "ARGV"
-ARGV.clear
-
 file.close
 
+
+#--------------- Using argument value (ARGV)
+
 file = File.open("/Users/basilalbader/dev/the_hacking_project/THP/week_3/05.Vendredi/Gemfile", "a")
+contenu = file.puts("source 'https://rubygems.org'\n")
+input = ARGV
 
-file.puts("gem '#{input}'")
+#note: never use "gets.chomp" and "ARGV" together as it will crash the program. 
+#In case you need both in your program (you moron!!!) make sure to use the following command after "ARGV"
+#===> ARGV.clear
 
+i = 0
+while i < input.length
+  file.puts("gem '#{input[i]}'")
+  i += 1
+end
 system("bundle install")
+file.close
